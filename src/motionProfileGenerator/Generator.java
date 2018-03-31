ffb940fbf1b48268b6e84c318be50a985aec9de0
reconnaisx <reconnaisx@github.com>
2018-03-30 21:13:45 -0700

    initial commit

---
+++ src/motionProfileGenerator/Generator.java
@@ -0,0 +1,46 @@
+package motionProfileGenerator;
+
+import java.io.File;
+
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.Waypoint;
+import jaci.pathfinder.modifiers.TankModifier;
+
+public class Generator {
+	
+	//CONSTANTS BANK: HIGH GEAR: VELOCITY = 15, ACCL = 3.2, LOW GEAR: VELOCITY = 7.25,ACCL = 6
+	
+	public static Waypoint[] points = {new Waypoint(0,0,0),
+								new Waypoint(10,0,0)};
+	public static double timeStep = 0.02;
+	public static double velocity = 7.25;
+	public static double acceleration = 6;
+	public static double jerk = 10;
+	public static double wheelBaseWidth = 2.25;
+	public static double wheelBaseDiameter = 5.875/12; //this is not relevant for tank drives
+	public static Trajectory.FitMethod fitMethod = Trajectory.FitMethod.HERMITE_CUBIC;
+	public static String filename = "STRAIGHT_LINE"; //sans the csv!
+	
+	public static Trajectory traj,left,right;
+	public static Trajectory.Config config;
+	public static TankModifier modifier;
+	
+	public static File leftOutput;
+	public static File rightOutput;
+	
+	public static void main (String[] args) {
+		config = new Trajectory.Config(fitMethod,Trajectory.Config.SAMPLES_HIGH,timeStep,velocity,acceleration,jerk);
+		traj = Pathfinder.generate(points, config);
+		modifier = new TankModifier(traj).modify(wheelBaseWidth);
+		
+		left = modifier.getLeftTrajectory();
+		right = modifier.getRightTrajectory();
+		
+		leftOutput = new File(filename + "_LEFT.csv");
+		rightOutput = new File(filename + "_RIGHT.csv");
+		
+		Pathfinder.writeToCSV(leftOutput, left);
+		Pathfinder.writeToCSV(rightOutput, right);
+	}
+}
